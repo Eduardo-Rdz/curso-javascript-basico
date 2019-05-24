0e2e2b365b13df8ad40028f3c7af95e5aff7354e
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-05-23 23:58:05 -0500

    2-variables

---
+++ 2-variables/1-variables.js
@@ -0,0 +1,26 @@
+// tipo cadena (string)
+var nombre = "Jon Snow";
+console.log("1:", nombre, " es mi amigo");
+
+// tipo numero (number)
+var numero = 5;
+console.log("2:", numero);
+
+// el contenido se actualiza
+numero = 3.14;
+console.log("3:", numero);
+
+// tipo string
+var numero2 = "24";
+console.log("4:", numero2);
+
+console.log("5:", typeof numero);
+console.log("6:", typeof numero2);
+
+// tipo boolean
+var tieneCuenta = true;
+console.log("7:", "Tiene cuenta?", tieneCuenta);
+console.log("8:", typeof tieneCuenta);
+
+tieneCuenta = false;
+console.log("9:", typeof tieneCuenta);
