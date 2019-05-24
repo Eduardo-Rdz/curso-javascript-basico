a0961895682f37328ec47b1e634605c4cb55bb4c
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-05-24 14:25:48 -0500

    5-null-undefined.js

---
+++ 2-variables/5-null-undefined.js
@@ -0,0 +1,17 @@
+var variableSinValor;
+console.log("1: variable sin asignacion", variableSinValor);
+
+var cadenaVacia = "";
+console.log("2: es un string?", typeof cadenaVacia);
+
+var humano = {
+  nombre: "saitama",
+  edad: 28
+};
+console.log("3: acceder a una propiedad inexistente", humano.correo);
+
+var animales = ["lobo", "leon"];
+console.log("4: acceder a una posicion inexistente", animales[2]);
+
+var valorNulo = null;
+console.log("5: vairable con valor nulo", valorNulo);
