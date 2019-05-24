fa6374e1bf07255a49661ebda546e7ecc31be947
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-05-24 00:51:02 -0500

    2-reglas.js

---
+++ 2-variables/2-regla.js
@@ -0,0 +1,35 @@
+var _numero = 2;
+console.log("1:", "puedes utilizar _", _numero);
+
+// no puedes comenzar con un numero
+// var 2numero = 2;
+
+// no utilizar palabras reservadas
+// var var;
+
+var var2 = true;
+console.log("2:", "puedes utilizar palabras reservadas si las modificas", var2);
+
+// var hola-mundo;
+
+var tiene_licencia = true;
+console.log(
+  "3:",
+  "unicamente puedes utilizar _ y caracteres alfanumericos",
+  tiene_licencia
+);
+
+// convenciones
+
+// 1: usar camel case
+var primerVariable = 2;
+var estoEsUnaVariable = 10;
+console.log("4:", "usar camel case", estoEsUnaVariable);
+
+// 2: declarar nombres con sentido
+var precioManzana = 5;
+console.log("5:", "el precio de la manzana es de $:", precioManzana);
+
+// 3: comenzar con minuscula
+var PrecioNaranja = 3;
+console.log("6:", "no comenzar con mayuscula", PrecioNaranja);
