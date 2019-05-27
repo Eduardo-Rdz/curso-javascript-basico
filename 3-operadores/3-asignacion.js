530cd81eca6d479ca81fc6c5d817fb1952a12b14
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-05-27 02:19:25 -0500

    3-asignacion.js

---
+++ 3-operadores/3-asignacion.js
@@ -0,0 +1,28 @@
+// asignacion basica operador =
+var x = 5;
+console.log("1:", x);
+
+// actualizar el valor de x usando su mismo valor
+// se resuelven primero los operadores aritmeticos
+x = x + 1;
+console.log("2:", x);
+
+// asignacion +=
+x += 2; // <= x = x + 2;
+console.log("3:", x);
+
+// asignacion -=
+x -= 2; // <= x = x - 2;
+console.log("4:", x);
+
+// asignacion *=
+x *= 2; // <= x = x * 2;
+console.log("5:", x);
+
+// asignacion /=
+x /= 2; // <= x = x / 2;
+console.log("6:", x);
+
+// asignacion %=
+x %= 2; // <= x = x % 2;
+console.log("7:", x);
