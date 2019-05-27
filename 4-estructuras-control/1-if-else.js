76f9d60bc41aef32f9359457c114bc3612876a2b
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-05-27 04:17:33 -0500

    1-if-else.js

---
+++ 4-estructuras-control/1-if-else.js
@@ -0,0 +1,47 @@
+if (false) {
+  console.log("1: Hola mundo!");
+} else {
+  console.log("1: Hola");
+}
+
+var hayDescuento = true;
+
+if (hayDescuento) {
+  console.log("2: Todavia hay descuentos!");
+}
+
+var horaTrabajo = false;
+
+if (horaTrabajo) {
+  console.log("3: A trabajar");
+} else {
+  console.log("3: A descansar");
+}
+
+// numeros
+if (2) {
+  console.log("4: con un numero que no sea 0 entra");
+} else {
+  console.log("4: es un 0");
+}
+
+// strings
+if ("hola") {
+  console.log("5: cadena no vacia");
+} else {
+  console.log("5: esta vacio");
+}
+
+// objetos
+if (null) {
+  console.log("5: es un objeto");
+} else {
+  console.log("5: es un null");
+}
+
+// undefined
+if (true) {
+  console.log("6: no es un undefined");
+} else {
+  console.log("6: es un undefined");
+}
