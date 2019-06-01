98df541730c7c90c358af0195348d5343cb6e680
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-06-01 18:44:51 -0500

    3-operadores-comparacion-2.js

---
+++ 4-estructuras-control/3-operadores-comparacion-2.js
@@ -0,0 +1,39 @@
+// operador igual que ==
+var persona = {
+  nombre: "Andy",
+  id: "1234",
+  edad: 20
+};
+
+if (persona.id == "1234") {
+  console.log("1: Los ids coinciden");
+} else {
+  console.log("1: Los ids no coinciden");
+}
+
+if (persona.edad == "20") {
+  console.log("2: Tiene 20 años");
+} else {
+  console.log("2: no tiene 20 años");
+}
+
+// operador igual que (tipo) ===
+if (persona.edad === "20") {
+  console.log("3: Tiene 20 años");
+} else {
+  console.log("3: no tiene 20 años");
+}
+
+// operador no igual que !=
+if (persona.id != 1234) {
+  console.log("4: todavia aplican descuentos para este usuario");
+} else {
+  console.log("4: ya no aplican descuentos para este usuario");
+}
+
+// operador no igual que (tipo) !==
+if (persona.id !== 1234) {
+  console.log("4: todavia aplican descuentos para este usuario");
+} else {
+  console.log("4: ya no aplican descuentos para este usuario");
+}
