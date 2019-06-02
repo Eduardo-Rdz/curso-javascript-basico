c5ed0fd7e61164c52c080c8efbeecf327d04cc33
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-06-02 03:08:18 -0500

    7-while-loop.js

---
+++ 4-estructuras-control/7-while-loop.js
@@ -0,0 +1,18 @@
+// var contador = 0;
+
+// while (contador < 101) {
+//   console.log("valor del contador", contador);
+//   contador++;
+// }
+
+var animales = ["mono", "jabali", "conejo", "serpiente"];
+console.log("longitud del arreglo", animales.length);
+
+var cantidadElementos = animales.length;
+
+var posicion = 0;
+
+while (posicion < cantidadElementos) {
+  console.log("posicion: ", posicion, animales[posicion]);
+  posicion++;
+}
