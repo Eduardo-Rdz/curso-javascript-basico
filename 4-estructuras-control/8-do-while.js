f17be929ed9fcdb795dc2052efbf3ff97a4d6981
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-06-03 02:51:47 -0500

    8-do-while.js

---
+++ 4-estructuras-control/8-do-while.js
@@ -0,0 +1,14 @@
+var contador = 10;
+
+do {
+  // codigo a repetir
+  console.log("do-while:", contador);
+  contador++;
+} while (contador < 10);
+
+var contador2 = 10;
+
+while (contador2 < 10) {
+  console.log("while:", contador2);
+  contador2++;
+}
