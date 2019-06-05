de2baf2006f724dcd94678e8d0a37e3425d508e9
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-06-04 21:28:06 -0500

    2-variables-locales-globales.js

---
+++ 5-algoritmos/2-variables-locales-globales.js
@@ -0,0 +1,17 @@
+var contador = 0;
+
+function incrementarContador() {
+  contador++;
+  console.log(contador);
+}
+
+incrementarContador();
+incrementarContador();
+
+function saludar() {
+  var saludoLocal = "hola mundo!";
+  console.log(saludoLocal);
+}
+
+// console.log(saludoLocal);
+saludar();
